7e63bea80a7ced4d1a305eaf0b282f74d14b35fd
jessyZheng1018 <jessy.zheng@pwc.com>
2016-09-07 23:43:19 -0400

    an example of toggle via ui bootstrap

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,24 @@
  * Controller of the angularUiBootstrapApp
  */
 angular.module('angularUiBootstrapApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.status = {
+      open: false,
+      isFirstDisabled: false,
+      isFirstOpen: false,
+    };
+    $scope.groups = [
+      {
+        title: 'Dynamic Group Header - 1',
+        content: 'Dynamic Group Body - 1'
+      },
+      {
+        title: 'Dynamic Group Header - 2',
+        content: 'Dynamic Group Body - 2'
+      }]
   });
